c6ee65552f00fcff1e318b24f236e534e0924f39
shOumik-saha <shoumiksahapranto@gmail.com>
2026-01-12 23:27:31 +0600

    Fix Navbar import casing issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { AboutSection } from "../components/AboutSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
-import { Navbar } from "../components/NavBar";
+import { Navbar } from "../components/Navbar";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
